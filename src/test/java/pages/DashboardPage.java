2955d09110441f8604b47f2d596490f6c9263032
Maycee19502 <142246230+Maycee19502@users.noreply.github.com>
2023-12-20 12:31:54 -0500

    update to dashboardPage

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -18,7 +18,35 @@ public class DashboardPage {
 	@FindBy(xpath = "//div[@class='header-links']/ul/li[2]")
 	public WebElement logoutButton;
 	
-	
+	@FindBy(xpath="//[@class='top-menu notmobile'] /li[1]/a") 
+    public WebElement Computers;
+
+    @FindBy(xpath="//[@class='top-menu notmobile'] /li[2]/a") 
+    public WebElement Electronics;
+
+    @FindBy(xpath="//[//[@class='top-menu notmobile'] /li[3]/a") 
+    public WebElement Apparel;
+
+    @FindBy(xpath="//[@class='top-menu notmobile'] /li[4]/a") 
+    public WebElement DigitalDownloads;
+
+    @FindBy(xpath="//[@class='top-menu notmobile'] /li[5]/a") 
+    public WebElement Books;
+
+    @FindBy(xpath="//[@class='top-menu notmobile'] /li[6]/a") 
+    public WebElement Jewelry;
+
+    @FindBy(xpath="//[@class='top-menu notmobile'] /li[7]/a") 
+    public WebElement GiftCards;
+
+    @FindBy(id="small-searchterm")
+    public WebElement SearchBar;
+
+    @FindBy(xpath="//[@type='submit']")
+    public WebElement SearchButton;
+
+    @FindBy(xpath="//[@class='header-logo']")
+    public WebElement Logo;
 	
 
 }
---
+++ src/test/java/testCases/testCase2.java
@@ -0,0 +1,13 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import utilities.CommonMethods;
+
+public class testCase2 extends CommonMethods{
+
+	@Test
+	public void testingFramework() {
+		System.out.println("this is just a test of the framework");
+	}
+}
