89c8d47d8c108159df392582849eb503051c913f
anums16 <anums6425@gmail.com>
2024-01-03 18:23:10 -0500

    Anum's test cases

---
+++ src/test/java/testCases/anum_banners/HomePage_Banner2.java
@@ -0,0 +1,31 @@
+package testCases.anum_banners;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utilities.BaseClass;
+import utilities.CommonMethods;
+
+public class HomePage_Banner2 extends CommonMethods{
+
+	@Test
+	public void VerifySecondBanner() {
+
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("demo store"));
+		click(hp.BuildYourOwnComputerButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("Build your own computer"));
+		click(hp.LogoButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("demo store"));
+		click(hp.AppleMacbookButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("Apple MacBook"));
+		click(hp.LogoButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("demo store"));
+		click(hp.HTCOneButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("HTC One"));
+		click(hp.LogoButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("demo store"));
+		click(hp.VirtualGiftCardButton);
+		Assert.assertTrue(BaseClass.getDriver().getTitle().contains("Virtual Gift Card"));
+
+	}
+}
