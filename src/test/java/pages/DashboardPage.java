4a7982dc2468578148a33aa259cbf16a2ea9b4b8
diluz <142556318+diluzzz@users.noreply.github.com>
2024-01-06 09:33:49 -0500

    Revert "diluz testcase"

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -73,16 +73,4 @@ public class DashboardPage {
 	
 	@FindBy(xpath="//a[contains(text(),'About nopCommerce')]")
 	public WebElement AboutNopCommerce;
-	
-	@FindBy(xpath = "//*[@class='master-wrapper-page']/div/div/div/div/select")
-	public WebElement customerCurrency;
-	
-	@FindBy(xpath = "//*[@class='header-links']/ul/li[3]/a/span[1]")
-	public WebElement Wishlist;
-	
-	@FindBy(xpath = "//*[@id=\"bar-notification\"]/div/span")
-	public WebElement Close;
-	
-	@FindBy(xpath = "//*[@class='master-wrapper-page']/div/div/div/div/select/option[2]")
-	public WebElement Euro;
 }
